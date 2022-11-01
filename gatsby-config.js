2d4ad0d1e23579941f868fe135db18c2e1b155c3
misha <35040017+MishaPetrov@users.noreply.github.com>
2022-10-31 22:58:51 -0700

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
       summary: `Find Your Story.`,
     },
     description: `Find Your Story.`,
-    siteUrl: `https://fictiontribe.com/`,
+    siteUrl: `https://blog.fictiontribe.com/`,
     social: {
       twitter: `fictiontribe`,
     },
@@ -25,6 +25,34 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-28921914-1', // Google Analytics / GA
+          // optional
+          'OPTIONAL----AW-CONVERSION_ID', // Google Ads / Adwords / AW
+          'OPTIONAL----DC-FLOODIGHT_ID', // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
       },
     },
     {
@@ -127,8 +155,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Fiction Tribe Blog`,
+        short_name: `Fiction Tribe`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
