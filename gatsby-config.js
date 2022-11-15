1349e83003df18d0ae01918b7eabbf4714f22ba7
misha <35040017+MishaPetrov@users.noreply.github.com>
2022-11-15 13:42:41 -0800

    update GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,14 +29,24 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        // The property ID; the tracking code won't be generated without it
-        trackingId: "314470500",
-      },
-      pluginConfig: {
-        head: true,
-        anonymize_ip: true,
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-28921914-14', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
       },
     },
     {
